187efd31b2700221d09444118580c3d1efb782c5
GodisE <117681935+GodisE@users.noreply.github.com>
2023-01-18 20:30:50 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+//command center HQ where all the important stuff happens
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const db = require("./config/db");
+const personRoutes = require("./routes/person_routes");
+
+const PORT = 8000;
+
+// new error from mongoose - deprication warning
+//this looks at the schema and if u try to create something that isnt
+//in your schema it rejects it
+mongoose.set("strictQuery", true);
+
+//connection string bw local db and this express app
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.use(cors({ origin: `http://127.0.0.1:5500` }));
+
+//sending json
+// need to be able to accept json
+app.use(express.json());
+
+app.use(personRoutes);
+
+app.listen(PORT, () => {
+  console.log("i hear u shawty");
+});
+
+module.exports = app;
